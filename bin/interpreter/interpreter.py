fdf7eb4c85fe9a7e9db26bef40d72194e45fdf60
oveof <ovedy@oftedal.com>
2020-08-07 22:22:26 +0200

    Updated Interpreter

--- bin/interpreter/interpreter.py
+++ bin/interpreter/interpreter.py
@@ -1,8 +1,28 @@
 from ics import Calendar
 import requests
+import json
+import arrow
 
-f = open("IELET100120v.ics", "r")
 
-c = Calendar(f.read())
+def getEvents():
+    f = open("IELET100120v.ics", "r")
+    c = Calendar(f.read())
 
-print(c.events)
+    events = []
+
+    for event in c.events:
+        event.begin = event.begin.replace(tzinfo='Europe/Oslo')
+        event.end = event.end.replace(tzinfo='Europe/Oslo')
+        eventObj = {
+            'summary': event.name,
+            'location': event.location,
+            'description': event.description,
+            'start': {
+                'dateTime': event.begin.format('YYYY-MM-DDTHH:MM:SSZZ')
+            }
+        }
+        print("\n")
+        print(eventObj)
+        
+
+getEvents()
